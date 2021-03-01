155b9b0e3447dce91ab60b83698e63faaf291931
rimma-fe <rimma.fedorova89@gmail.com>
2021-03-01 15:03:27 +0200

    вносит изменения в секции

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+(() => {
+    const menuBtnRef = document.querySelector('[data-menu-button]');
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+  })
+})();
